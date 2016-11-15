14c53bc87f265b6cd5f1785bf62741f75eb2fd21
Pauls <peerkons96@gmail.com>
2016-11-15 16:32:34 +0200

    ef

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-jQuery(document).ready(function()
-{
-var navOffset = jQuery("site-header").offset().top;
-alert(navOffset);
-
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 280) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
 });
