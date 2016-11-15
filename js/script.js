be44ae4e4a6ca7e39072a480d9db0cee94297b78
Pauls <peerkons96@gmail.com>
2016-11-15 23:23:56 +0200

    wd

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function()
-{
-var navOffset = jQuery("site-header").offset().top;
+jQuery(document).ready(function() {
+
+	var navOffset = jQuery("site-header").offset().top;
 
 jQuery(window).scroll(function() {
 var scrollPos = jQuery(window).scrollTop();
-jQuery(".status").html(scrollPos);
+jQuery("status").html(scrollPos);
 });
 
 
