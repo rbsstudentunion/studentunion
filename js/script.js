b67f1479db77f98c96d161003cc459fe1d2739d1
Pauls <peerkons96@gmail.com>
2016-11-15 16:41:33 +0200

    Revert "ef"
    This reverts commit 14c53bc87f265b6cd5f1785bf62741f75eb2fd21.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
-$(document).ready(function() {
-  
-  $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
-      console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 280) {
-      $('#nav_bar').addClass('navbar-fixed');
-    }
-    if ($(window).scrollTop() < 281) {
-      $('#nav_bar').removeClass('navbar-fixed');
-    }
-  });
+jQuery(document).ready(function()
+{
+var navOffset = jQuery("site-header").offset().top;
+alert(navOffset);
+
 });
