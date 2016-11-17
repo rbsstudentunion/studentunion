12139433f59fe75024db0c2e8d7b0de99aa14fb6
Pauls <peerkons96@gmail.com>
2016-11-17 09:26:29 +0200

    ef

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-var  mn = $(".site-header");
-    mns = "main-nav-scrolled";
-    hdr = $('header').height();
-
+$(document).ready(function() {
+var stickyNavTop = $('.site-header').offset().top;
+ 
+var stickyNav = function(){
+var scrollTop = $(window).scrollTop();
+      
+if (scrollTop > stickyNavTop) { 
+    $('.site-header').addClass('sticky');
+} else {
+    $('.site-header').removeClass('sticky'); 
+}
+};
+ 
+stickyNav();
+ 
 $(window).scroll(function() {
-  if( $(this).scrollTop() > hdr ) {
-    mn.addClass(mns);
-  } else {
-    mn.removeClass(mns);
-  }
+  stickyNav();
+});
 });
