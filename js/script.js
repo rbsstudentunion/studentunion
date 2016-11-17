92805cc55f7cf033e20b0d961a3d640a7bce1046
Pauls <peerkons96@gmail.com>
2016-11-17 10:20:10 +0200

    ef

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-window.onload = function() {
-    if (window.jQuery) {  
-        // jQuery is loaded  
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-}
-
-
-
 
 
 
