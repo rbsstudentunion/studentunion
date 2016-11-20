a69580f3d924ea40dec61e9f16797582fdc7a213
Pauls <peerkons96@gmail.com>
2016-11-20 17:09:02 +0200

    we

--- js/script.js
+++ js/script.js
@@ -1,23 +1,16 @@
-
-
-
-
-$(document).ready(function() {
-var stickyNavTop = $('.site-header').offset().top;
- 
-var stickyNav = function(){
-var scrollTop = $(window).scrollTop();
-      
-if (scrollTop > stickyNavTop) { 
-    $('.site-header').addClass('sticky');
-} else {
-    $('.site-header').removeClass('sticky'); 
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#content-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#site-header').addClass('stick');
+        $('#site-header-phantom').show();
+    } else {
+        $('#site-header').removeClass('stick');
+        $('#site-header-phantom').hide();
+    }
 }
-};
- 
-stickyNav();
- 
-$(window).scroll(function() {
-  stickyNav();
-});
+
+$(function () {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
 });
