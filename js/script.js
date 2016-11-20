8503eb9604f79d68267fa27f7ec88f511bc29564
Pauls <peerkons96@gmail.com>
2016-11-20 21:34:08 +0200

    rw

--- js/script.js
+++ js/script.js
@@ -23,19 +23,3 @@ $(window).scroll(function() {
 });
 
 
-var amountScrolled = 300;
-
-$(window).scroll(function() {
-	if ( $(window).scrollTop() > amountScrolled ) {
-		$('a.back-to-top').fadeIn('slow');
-	} else {
-		$('a.back-to-top').fadeOut('slow');
-	}
-});
-
-$('a.back-to-top').click(function() {
-	$('html, body').animate({
-		scrollTop: 0
-	}, 700);
-	return false;
-});
