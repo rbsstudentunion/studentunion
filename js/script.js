fa966b835db05267dd1b49b5969b3ec9c562de87
Pauls <peerkons96@gmail.com>
2016-12-15 01:34:51 +0200

    Revert "b"
    This reverts commit 58b73baba0e5b267dcfba05dc951c7a0831f22ee.

--- js/script.js
+++ js/script.js
@@ -26,7 +26,6 @@ $(window).scroll(function() {
 // Get the modal
 var modal = document.getElementById('myModal');
 
-var modal1 = document.getElementById('myModal1');
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
@@ -38,26 +37,15 @@ document.getElementById("myBtn").onclick = function() {
     modal.style.display = "block";
 }
 
-document.getElementById("myBtn").onclick = function() {
-    modal1.style.display = "block";
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-span.onclick = function() {
-    modal1.style.display = "none";
-}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
-    }
-
-    window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal1.style.display = "none";
     }
 }
 
