0fd286c99e63240b9c6ebd3493a71edb8b4c652d
Pauls <peerkons96@gmail.com>
2016-12-15 22:55:13 +0200

    Revert "q"
    This reverts commit 83625e6baa9818a0980087abbd594437edea4c4b.

--- js/script.js
+++ js/script.js
@@ -76,8 +76,8 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > 5) {slideIndex = 0} 
-  if (n < 0) {slideIndex = 5}
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
