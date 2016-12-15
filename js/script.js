7c54eeb839d281941dbb797b2255cdc4dfe1a615
Pauls <peerkons96@gmail.com>
2016-12-15 20:55:51 +0200

    qef

--- js/script.js
+++ js/script.js
@@ -76,8 +76,8 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slideIndex = 0}    
+  if (n < 0) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
