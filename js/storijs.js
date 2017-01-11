bea46c7ee036ee442d649b684b4c8d3753372e22
Pauls <peerkons96@gmail.com>
2017-01-12 00:13:23 +0200

    qef

--- js/storijs.js
+++ js/storijs.js
@@ -1,10 +1,19 @@
 
 
+var ul = document.getElementById('virsraksts');
+
+ul.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI'){
+      document.getElementById("kontents").classList.toggle("para");
+    }
+});
 
 
-document.querySelectorAll("slid").onclick = function() {
-	myFunction()};
 
-function myFunction() {
-    document.getElementById("kontents").classList.toggle("para");
-}
+
+//document.querySelectorAll("slid").onclick = function() {
+//	myFunction()};
+
+//function myFunction() {
+//    document.getElementById("kontents").classList.toggle("para");
+//}
