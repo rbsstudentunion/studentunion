839ee1af47cf365df4c7ddb82def766e3c50d1fb
Pauls <peerkons96@gmail.com>
2017-01-12 01:00:39 +0200

    vf

--- js/storijs.js
+++ js/storijs.js
@@ -6,3 +6,9 @@ document.getElementById("virsraksts").onclick = function() {
 function myFunction() {
     document.getElementById("kontents").classList.toggle("para");
 }
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideDown("slow");
+    });
+});
