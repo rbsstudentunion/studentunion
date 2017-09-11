0c1a6fb8131e447dde33e543964a0ddafc57fa26
Pauls Perkons <student.union@rbs.lv>
2017-09-11 08:59:48 +0000

    Updated 'js/script.js' via CloudCannon

--- js/script.js
+++ js/script.js
@@ -88,3 +88,70 @@ function showSlides(n) {
 }
 
 //bilde merch beingas
+
+
+// Get the modal
+var modal = document.getElementById('myModal1');
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+var close = document.getElementById("close");
+
+var bigclose = document.getElementById("bigclose");
+// When the user clicks the button, open the modal 
+
+  document.getElementById("myBtn1").onclick = function() {
+    modal.style.display = "block";
+  }
+
+
+
+
+// When the user clicks on <span> (x), close the modal
+close.onclick = function() {
+    modal.style.display = "none";
+}
+
+bigclose.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//bilde merch
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+//bilde merch beingas
