30f06040601edec88c39c5fb5a74de90c3ce546f
Pauls Perkons <student.union@rbs.lv>
2017-09-14 08:51:20 +0000

    Updated 'js/script.js' via CloudCannon

--- js/script.js
+++ js/script.js
@@ -24,45 +24,57 @@ $(window).scroll(function() {
 
 
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal1 = document.getElementById('myModal-1');
+var modal2 = document.getElementById('myModal-2');
 
-var merchpic = document.getElementById("merchpic");
-var merchpic1 = document.getElementById("merchpic1");
 // Get the button that opens the modal
+var btn1 = document.getElementById("modalBtn-1");
+var btn2 = document.getElementById("modalBtn-2");
 
 // Get the <span> element that closes the modal
-var close = document.getElementById("close");
+var span1 = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
 
-var bigclose = document.getElementById("bigclose");
-// When the user clicks the button, open the modal 
+var bigspan1 = document.getElementsByClassName("bigclose")[0];
+var bigspan2 = document.getElementsByClassName("bigclose")[1];
 
-merchpic.onclick = function() {
-    modal.style.display = "block";
+// When the user clicks on the button, open the modal
+btn1.onclick = function() {
+    modal1.style.display = "block";
 }
-merchpic1.onclick = function() {
-    modal.style.display = "block";
+
+btn2.onclick = function() {
+    modal2.style.display = "block";
 }
-
-
-
 
 // When the user clicks on <span> (x), close the modal
-close.onclick = function() {
-    modal.style.display = "none";
+span1.onclick = function() {
+    modal1.style.display = "none";
 }
 
-bigclose.onclick = function() {
-    modal.style.display = "none";
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+bigspan1.onclick = function() {
+    modal1.style.display = "none";
+}
+
+bigspan2.onclick = function() {
+    modal2.style.display = "none";
 }
 
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+    if( event.target == modal2){
+    		modal2.style.display = "none";
     }
 }
-
 //bilde merch
 
 var slideIndex = 1;
@@ -93,16 +105,6 @@ function showSlides(n) {
 }
 
 //bilde merch beigas
-
-// Get the modal
-var modal1 = document.getElementById('myModal1');
-
-// Get the button that opens the modal
-
-
-  document.getElementById("merchpic1").onclick = function() {
-    modal1.style.display = "block";
-  }
 
 
 
